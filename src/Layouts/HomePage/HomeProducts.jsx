c3ff453761c5676712d042f5f90f9893f6e3abf6
nahiyankhan55 <kushbuladesignmax+++@gmail.com>
2025-11-10 12:48:16 +0600

    home latest products section add

--- src/Layouts/HomePage/HomeProducts.jsx
+++ src/Layouts/HomePage/HomeProducts.jsx
@@ -1,5 +1,72 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../Hook/useAxiosPublic";
+import DataLoader from "../../Components/DataLoader";
+import { Rating } from "@mui/material";
+import { Link } from "react-router";
+
 const HomeProducts = () => {
-  return <div>HomeProducts</div>;
+  const AxiosPublic = useAxiosPublic();
+  const { data: products = [], isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await AxiosPublic.get("/products/home/latest");
+      return res.data.data;
+    },
+    retry: 3,
+    retryDelay: 2000,
+  });
+  console.log(products);
+  if (isLoading) return <DataLoader></DataLoader>;
+
+  return (
+    <div className="max-w-6xl mx-auto py-10 px-5">
+      <h1 className="text-3xl font-bold text-center">Latest Products</h1>
+      <p className="text-gray-600 text-center font-medium max-w-2xl mx-auto mt-2">
+        Latest products from trusted sellers, updated in real time so you always
+        see the freshest, most in-demand items ready for global shipment.
+      </p>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 mt-8">
+        {products.map((product) => (
+          <div
+            key={product._id}
+            className="border rounded-lg shadow-md overflow-hidden hover:shadow-lg hover:shadow-gray-400 transition"
+          >
+            <img
+              src={product.image}
+              alt={product.name}
+              className="w-full h-48 object-cover shadow-md"
+            />
+            <div className="p-4 flex flex-col gap-2">
+              <h2 className="font-semibold text-lg">{product.name}</h2>
+              <p>Price: ${product.price}</p>
+              <p>Origin: {product.origin}</p>
+              <Rating value={product.rating} precision={0.1} readOnly />
+              <p>
+                Available Quantity:{" "}
+                <span
+                  className={`font-medium ${
+                    product.quantity > 0
+                      ? product.quantity > 10
+                        ? "text-green-600"
+                        : "text-orange-600"
+                      : "text-red-600"
+                  }`}
+                >
+                  {product.quantity}
+                </span>
+              </p>
+              <Link
+                to={`/product/${product._id}`}
+                className="mt-2 text-center bg-blue-500 text-white py-1 rounded hover:bg-blue-600 transition"
+              >
+                See Details
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomeProducts;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import "./index.css";
 import WebRouter from "./WebRouter/WebRouter.jsx";
 import WebContextProvider from "./Context/WebContextProvider.jsx";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <WebContextProvider>
-      <WebRouter></WebRouter>
-      <ToastContainer></ToastContainer>
-    </WebContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <WebContextProvider>
+        <WebRouter></WebRouter>
+        <ToastContainer></ToastContainer>
+      </WebContextProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
